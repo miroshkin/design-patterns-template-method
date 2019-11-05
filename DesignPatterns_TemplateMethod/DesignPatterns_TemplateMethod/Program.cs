0f9c58b08d923da6865bacf6ec410274927a0b8a
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-05 11:04:53 +0300

    Added example

--- DesignPatterns_TemplateMethod/DesignPatterns_TemplateMethod/Program.cs
+++ DesignPatterns_TemplateMethod/DesignPatterns_TemplateMethod/Program.cs
@@ -6,7 +6,63 @@ namespace DesignPatterns_TemplateMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Design Patterns - Template Method");
+
+            McDonaldsBurger burgerOne = new McDonaldsBurger();
+            KFCBurger burgerTwo = new KFCBurger();
+
+            Console.WriteLine($"Burger One Price ({burgerOne.GetType()}) is {burgerOne.GetBurgerPrice()}");
+            Console.WriteLine($"Burger Two Price ({burgerTwo.GetType()})is {burgerTwo.GetBurgerPrice()}");
         }
     }
+
+    public abstract class PriceCalculator
+    {
+        //Template method
+        public decimal GetBurgerPrice()
+        {
+            var bread = GetBreadPrice();
+            var meat = GetMeatPrice();
+            var salad = GetSaladPrice();
+
+            return bread + meat + salad;
+        }
+
+        private decimal GetBreadPrice()
+        {
+            return 10;
+        }
+
+        public abstract decimal GetMeatPrice();
+
+        public abstract decimal GetSaladPrice();
+
+    }
+
+    public class McDonaldsBurger : PriceCalculator
+    {
+        public override decimal GetMeatPrice()
+        {
+            return 30;
+        }
+
+        public override decimal GetSaladPrice()
+        {
+            return 20;
+        }
+    }
+
+    public class KFCBurger : PriceCalculator
+    {
+        public override decimal GetMeatPrice()
+        {
+            return 40;
+        }
+
+        public override decimal GetSaladPrice()
+        {
+            return 20;
+        }
+    }
+
 }
